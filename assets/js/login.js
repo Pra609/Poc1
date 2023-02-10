3d8c13da79f88c8ca0c10bb73056693dbe1e54c3
Pragya0600 <pragya@mail.vinove.com>
2023-02-10 17:36:28 +0530

    commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,105 +1,32 @@
+var loginValidated=false;
+ 
 
- function printError(elemId, hintMsg) {
-    document.getElementById(elemId).innerHTML = hintMsg;
-}
-
-
-function signup(){
+function login(){
 
     
  
     var email =document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-   
-   
-	// Defining error variables with a default value
-    
-   
-    var emailErr = true;
-    
-    var passwordErr = true;
-   
-   
-       console.log(email)
-        console.log(password)
-    // Validate name
-   
-    
-    // Validate email address
-    if(email == "") {
-        printError("emailErr", "Please enter your email address");
-    } else {
-        // Regular expression for basic email validation
-        var regex = /^\S+@\S+\.\S+$/;
-        if(regex.test(email) === false) {
-            printError("emailErr", "Please enter a valid email address");
-        } else{
-            printError("emailErr", "");
-            emailErr = false;
-        }
-    }
-    
-   
-    
-//    // Validate role
-//    if(role == "Select") {
-//        printError("roleErr", "Please select your role");
-//    } else {
-//        printError("roleErr", "");
-//        roleErr = false;
-//    }
-  
-  // validate password
-  
-    if(password == "") {
-        printError("passwordErr", "Please enter your password");
-    } else {
-        var regex = /^[a-zA-Z1-9\s]+$/;                
-        if(regex.test(password) === false) {
-            printError("passwordErr", "Please enter a valid password");
-        } else {
-            printError("passwordErr", "");
-            passwordErr = false;
-        }
+    //console.log(email+"email");
+
+    var email1=localStorage.getItem("email");
+    var password1=localStorage.getItem("password");
+
+    if(email1.match(email) && password1.match(password)){
+        loginValidated=true;
     }
   
-  
-    
-    
-    // Prevent the form from being submitted if there are any errors
-    if((emailErr || passwordErr) == true) {
-       console.log(nameErr)       
-       return false;
-    } else {
-        // Creating a string from input data for preview
-        var dataPreview = "You've entered the following details: \n" +
-                          
-                           "Email Address: " + email + "\n" +
-                        
-            
-                         
-             
-       console.log(dataPreview);
-        // Display input data in a dialog box before submitting the form
-     
-	   
-	   alert(dataPreview);
-	   
-	   validated =true;
-       if(validated=true){
-        
-        localStorage.setItem("email",email);
-        localStorage.setItem("password",password);
-    
-        console.log(name+".....");
-    
-    }
-	  
-      
-    return true;       
- 
-}
+      console.log(loginValidated+"loginvalidated");
+
+
+   if(loginValidated.match(true)){
+    alert("login successfull");
+    window.location = "gallery.html";
+   }
+
+   
+	
 
     
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,8 @@ function signup(){
     
     var name = document.getElementById("name").value;
     var contact = document.getElementById("contact").value;
-    var email =document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+    var email =document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
    
    
